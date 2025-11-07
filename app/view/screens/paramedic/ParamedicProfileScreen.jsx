1148ef10a1db25555466731c22ddb368677a445e
Ana Paola Jiménez Sedano <143899647+AnaJmz345@users.noreply.github.com>
2025-11-07 11:41:50 -0600

    "configure all the main screens for every user"

---
+++ app/view/screens/paramedic/ParamedicProfileScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
+
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
+import TopMenu from '../../components/TopMenu'
+export default function ParamedicProfileScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+
+      <ScrollView contentContainerStyle={styles.content}>
+        <TopMenu navigation ={navigation}></TopMenu>
+        
+      </ScrollView>
+    </View>
+  );
+}
