cf4b297b1ba498dd204444b6aa12794893233aad
cesarmran <a01645209@tec.mx>
2025-11-23 14:57:33 -0600

    Sign out every time app loads for tests

--- App.js
+++ App.js
@@ -4,11 +4,16 @@ import { AuthProvider, useAuth } from './app/view/context/AuthContext';
 import AppNavigator from './app/view/navigation/AppNavigator';
 import { initLocalDB } from './app/view/lib/initLocalDB';
 import { downloadProfileFromSupabase, syncPendingProfile } from "./app/view/lib/syncProfile";
+import { supabase } from "./app/view/lib/supabase";
 
 
 function AppContent() {
   const { user } = useAuth();
 
+  useEffect(() => {
+    supabase.auth.signOut();
+  }, []);
+  
   useEffect(() => {
     const init = async () => {
       await initLocalDB();
